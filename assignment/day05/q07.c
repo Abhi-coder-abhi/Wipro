e0fb3ad26fb13ca95ce13f3d9c2b49f5b14bc890
Abhishek <tiwariabhi1406@gmail.com>
2024-04-17 12:11:34 +0530

    commit

--- assignment/day05/q07.c
+++ assignment/day05/q07.c
@@ -2,19 +2,31 @@
 #include <math.h>
 int main()
 {
-    
-	int x ;
-scanf("%d",&x);
-if(x<2 || x>10)
-{
-    printf("not valid input");
-}
-	for (int i=1; i <=x; i++)
-	{
-      for(int j=1; j<=i; j++){
-printf("* ");
-      }
-      printf("\n");
-	}
-	return 0;
+
+    int x;
+    scanf("%d", &x);
+    if (x < 2 || x > 10)
+    {
+        printf("not valid input");
+    }
+    else
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                printf("* ");
+            }
+            printf("\n");
+        }
+         for (int i = x-1; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
 }
